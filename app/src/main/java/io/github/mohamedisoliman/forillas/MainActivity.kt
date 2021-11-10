7e32f6d3d3b04b03ad8e3b2166f2c79f7d96d213
Mohamed Ibrahim <mibrahim.dev@gmail.com>
2021-11-11 01:26:30 +0200

    Fix RemoteFeed Compile Error Unbound symbols not allowed

--- app/src/main/java/io/github/mohamedisoliman/forillas/MainActivity.kt
+++ app/src/main/java/io/github/mohamedisoliman/forillas/MainActivity.kt
@@ -8,11 +8,30 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import io.github.mohamedisoliman.forillas.data.FeedRepository
+import io.github.mohamedisoliman.forillas.data.apollo.apolloClient
+import io.github.mohamedisoliman.forillas.data.apollo.okhttpClient
+import io.github.mohamedisoliman.forillas.data.remote.RemoteFeed
 import io.github.mohamedisoliman.forillas.ui.theme.ForillasTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import logcat.logcat
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), CoroutineScope by CoroutineScope(Dispatchers.Default) {
+
+    val feedRepository by lazy {
+        FeedRepository(RemoteFeed(apolloClient(okhttpClient())))
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        feedRepository.retrieveAllPosts()
+            .onEach { logcat { it.toString() } }
+            .launchIn(this)
+
         setContent {
             ForillasTheme {
                 // A surface container using the 'background' color from the theme
--- app/src/main/java/io/github/mohamedisoliman/forillas/data/remote/RemoteFeed.kt
+++ app/src/main/java/io/github/mohamedisoliman/forillas/data/remote/RemoteFeed.kt
@@ -3,7 +3,7 @@ package io.github.mohamedisoliman.forillas.data.remote
 import com.apollographql.apollo.ApolloClient
 import com.apollographql.apollo.api.Input
 import com.apollographql.apollo.coroutines.await
-import io.github.mohamedisoliman.forillas.AllPostsQuery
+import io.github.mohamedisoliman.forillas.GetAllPostsQuery
 import io.github.mohamedisoliman.forillas.GetPostQuery
 import io.github.mohamedisoliman.forillas.data.entities.FeedPost
 import io.github.mohamedisoliman.forillas.data.entities.PageOptions
@@ -14,29 +14,35 @@ import kotlinx.coroutines.flow.flow
 class RemoteFeed(private val apolloClient: ApolloClient) : RemoteFeedContract {
 
     override fun retrieveAllPosts(options: PageOptions): Flow<List<FeedPost>> = flow {
-
+//
         val input = Input.optional(PageQueryOptions())
-        val response = apolloClient.query(AllPostsQuery(input)).await()
-        val posts = response.data?.posts?.data?.mapNotNull { it?.toFeedPost() }
+        val response = apolloClient.query(GetAllPostsQuery(input)).await()
+        if (response.hasErrors()) {
+            throw RuntimeException(response.errors?.firstOrNull()?.message)
+        } else {
+            val posts = response.data?.posts?.data?.mapNotNull {
+                FeedPost(body = it?.body, title = it?.title, id = it?.id)
+            }
+            emit(posts ?: emptyList<FeedPost>())
 
-        emit(posts ?: emptyList())
+        }
+
     }
 
     override fun retrievePost(id: Long): Flow<FeedPost> = flow {
         val response = apolloClient.query(GetPostQuery(id.toString())).await()
-        val post = response.data?.post?.toFeedPost()
-        emit(post ?: FeedPost())
+        if (response.hasErrors()) {
+            throw RuntimeException(response.errors?.firstOrNull()?.message)
+        } else {
+            val post =
+                response.data?.post?.let {
+                    FeedPost(
+                        body = it.body,
+                        title = it.title,
+                        id = it.id
+                    )
+                }
+            emit(post ?: FeedPost())
+        }
     }
 }
-
-private fun GetPostQuery.Post.toFeedPost() = FeedPost(
-    id = this.id,
-    title = this.title,
-    body = this.body
-)
-
-private fun AllPostsQuery.Data1.toFeedPost() = FeedPost(
-    id = this.id,
-    title = this.title,
-    body = this.body
-)
