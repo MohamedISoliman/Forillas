57064dba736ad1745da62adff643e43cfd3ae8ed
Mohamed Ibrahim <mibrahim.dev@gmail.com>
2021-11-11 03:04:30 +0200

    Test RetrieveAllPosts interactor

--- app/src/main/java/io/github/mohamedisoliman/forillas/domain/RetrieveAllPosts.kt
+++ app/src/main/java/io/github/mohamedisoliman/forillas/domain/RetrieveAllPosts.kt
@@ -3,17 +3,34 @@ package io.github.mohamedisoliman.forillas.domain
 import io.github.mohamedisoliman.forillas.data.FeedRepository
 import io.github.mohamedisoliman.forillas.data.entities.FeedPost
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.*
 
 class RetrieveAllPosts(
     private val feedRepository: FeedRepository,
-    private val dispatcher: CoroutineDispatcher,
-) : () -> Flow<List<FeedPost>> {
+    private val dispatcher: CoroutineDispatcher = Dispatchers.IO,
+) : () -> Flow<HomeState> {
 
-    override fun invoke(): Flow<List<FeedPost>> {
-        return feedRepository.retrieveAllPosts().flowOn(dispatcher)
+    override fun invoke(): Flow<HomeState> {
+        return feedRepository.retrieveAllPosts()
+            .map {
+                if (it.isEmpty()) HomeState.EmptyResult
+                else HomeState.Success(result = it)
+            }
+            .catch { emit(HomeState.Failure(it)) }
+            .onStart { emit(HomeState.Loading) }
+            .flowOn(dispatcher)
     }
+}
 
+sealed class HomeState(
+    val isLoading: Boolean = false,
+    val postsList: List<FeedPost>? = null,
+    val throwable: Throwable? = null,
+) {
+    object EmptyResult : HomeState(postsList = emptyList())
+    object Loading : HomeState(isLoading = true)
+    data class Success(val result: List<FeedPost>) : HomeState(postsList = result)
+    data class Failure(val error: Throwable) : HomeState(throwable = error)
 
 }
---
+++ app/src/test/java/io/github/mohamedisoliman/forillas/domain/RetrieveAllPostsTest.kt
@@ -0,0 +1,88 @@
+package io.github.mohamedisoliman.forillas.domain
+
+import io.github.mohamedisoliman.forillas.data.FeedRepository
+import io.github.mohamedisoliman.forillas.data.entities.FeedPost
+import io.github.mohamedisoliman.forillas.data.remote.RemoteFeed
+import io.mockk.every
+import io.mockk.mockk
+import kotlinx.coroutines.flow.drop
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.*
+import org.junit.Test
+
+class RetrieveAllPostsTest {
+
+
+    @Test
+    fun `WHEN RetrieveAllPosts() invoked THEN start with Loading state`(): Unit = runBlocking {
+        //GIVEN
+        val remote = mockk<RemoteFeed>()
+        val repo = FeedRepository(remote)
+        val interactor = RetrieveAllPosts(feedRepository = repo)
+
+        //WHEN
+        every { remote.retrieveAllPosts() } returns flowOf(emptyList())
+
+        //THEN
+        val first = interactor.invoke().first()
+        assertTrue(first is HomeState.Loading)
+    }
+
+    @Test
+    fun `GIVEN remote return emptyList WHEN RetrieveAllPosts() invoked THEN return Empty state`(): Unit =
+        runBlocking {
+            //GIVEN
+            val remote = mockk<RemoteFeed>()
+            val repo = FeedRepository(remote)
+            val interactor = RetrieveAllPosts(feedRepository = repo)
+            every { remote.retrieveAllPosts() } returns flowOf(emptyList())
+
+            //WHEN
+            val first = interactor.invoke().drop(1).first()
+
+            //THEN
+            assertTrue(first is HomeState.EmptyResult)
+        }
+
+    @Test
+    fun `GIVEN remote return result WHEN RetrieveAllPosts() invoked THEN return Success state`(): Unit =
+        runBlocking {
+            //GIVEN
+            val post = mockk<FeedPost>()
+            val remote = mockk<RemoteFeed>()
+            val repo = FeedRepository(remote)
+            val interactor = RetrieveAllPosts(feedRepository = repo)
+            val list = listOf(post, post, post)
+            every { remote.retrieveAllPosts() } returns flowOf(list)
+
+            //WHEN
+            val first = interactor.invoke().drop(1).first()
+
+            //THEN
+            assertTrue(first is HomeState.Success && first.result.size == list.size)
+        }
+
+
+    @Test
+    fun `GIVEN remote throw error WHEN RetrieveAllPosts() invoked THEN return Failure state`(): Unit =
+        runBlocking {
+            //GIVEN
+            val remote = mockk<RemoteFeed>()
+            val repo = FeedRepository(remote)
+            val interactor = RetrieveAllPosts(feedRepository = repo)
+            val message = "Some error happened"
+            every { remote.retrieveAllPosts() } returns flow { throw RuntimeException(message) }
+
+            //WHEN
+            val first = interactor.invoke().drop(1).first()
+
+            //THEN
+            assertTrue((first as HomeState.Failure).error is RuntimeException)
+            assertTrue(first.error.message == message)
+        }
+
+
+}
