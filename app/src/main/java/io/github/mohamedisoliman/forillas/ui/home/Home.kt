a2f58897e371971518e66a2c723c008dafb76076
Mohamed Ibrahim <mibrahim.dev@gmail.com>
2021-11-13 03:08:29 +0200

    Handle home screen view states

--- app/src/main/java/io/github/mohamedisoliman/forillas/ui/home/Home.kt
+++ app/src/main/java/io/github/mohamedisoliman/forillas/ui/home/Home.kt
@@ -2,51 +2,102 @@ package io.github.mohamedisoliman.forillas.ui.home
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material.icons.outlined.Warning
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import io.github.mohamedisoliman.forillas.R
 import io.github.mohamedisoliman.forillas.data.entities.FeedPost
+import io.github.mohamedisoliman.forillas.domain.HomeState
+import io.github.mohamedisoliman.forillas.ui.common.PlaceHolderView
 
 
 @Composable
 fun Home(viewModel: HomeViewModel) {
     val viewState = viewModel.state().collectAsState()
-    val postsList = viewState.value.postsListWithBodyTruncated
 
-    if (postsList != null && postsList.isNotEmpty()) {
-        PostsListView(list = postsList)
+    val value: HomeState = viewState.value
+    HomeContent(value)
+}
+
+@Composable
+private fun HomeContent(
+    state: HomeState,
+) {
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        LoadingView(modifier = Modifier.align(Alignment.Center), isLoading = state.isLoading)
+        PostsListView(list = state.postsList)
+        EmptyView(list = state.postsList)
+        ErrorView(throwable = state.throwable)
     }
 }
 
 @Composable
-fun PostsListView(modifier: Modifier = Modifier, list: List<FeedPost>) {
-    LazyColumn(
-        modifier = modifier
-            .fillMaxWidth()
-            .padding(16.dp),
+fun EmptyView(modifier: Modifier = Modifier, list: List<FeedPost>?) {
+    if (list?.isEmpty() == true) {
+        PlaceHolderView(modifier = modifier, icon = Icons.Outlined.Info,
+            message = stringResource(R.string.emptyViewMessage))
+    }
+}
 
-        verticalArrangement = Arrangement.SpaceBetween
-    ) {
+@Composable
+fun ErrorView(modifier: Modifier = Modifier, throwable: Throwable?) {
+    throwable?.let {
+        PlaceHolderView(
+            modifier = modifier, icon = Icons.Outlined.Warning,
+            message = throwable.message ?: stringResource(R.string.defaultErrorMessage),
+            textColor = MaterialTheme.colors.error
+        )
+    }
+}
 
-        list.forEachIndexed { index, feedPost ->
-            item {
-                PostView(item = feedPost) {
-                    //TODO:navigate to Details
-                }
-            }
-            if (index < list.lastIndex)
+@Composable
+fun LoadingView(modifier: Modifier = Modifier, isLoading: Boolean?) {
+    if (isLoading == true) {
+        CircularProgressIndicator(
+            modifier = modifier
+                .size(48.dp)
+        )
+    }
+}
+
+@Composable
+fun PostsListView(modifier: Modifier = Modifier, list: List<FeedPost>?) {
+    if (list?.isNotEmpty() == true) {
+        LazyColumn(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+
+            list.forEachIndexed { index, feedPost ->
                 item {
-                    Divider(thickness = 1.dp)
+                    PostView(item = feedPost) {
+                        //TODO:navigate to Details
+                    }
                 }
-        }
+                if (index < list.lastIndex)
+                    item {
+                        Divider(thickness = 1.dp)
+                    }
+            }
 
+        }
     }
 }
 
