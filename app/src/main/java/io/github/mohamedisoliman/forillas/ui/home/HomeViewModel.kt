99f4a1c2deb0d4d47f2a8542cac8820867dd9db9
Mohamed Ibrahim <mibrahim.dev@gmail.com>
2021-11-13 15:41:23 +0200

    clean import

--- app/src/main/java/io/github/mohamedisoliman/forillas/ui/home/HomeViewModel.kt
+++ app/src/main/java/io/github/mohamedisoliman/forillas/ui/home/HomeViewModel.kt
@@ -8,7 +8,6 @@ import io.github.mohamedisoliman.forillas.data.entities.FeedPost
 import io.github.mohamedisoliman.forillas.domain.HomeState
 import io.github.mohamedisoliman.forillas.domain.RetrieveAllPosts
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
